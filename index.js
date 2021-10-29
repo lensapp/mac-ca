bec5c56e1b7916cec895d2b49b3526bebb5111e4
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2021-10-29 10:49:28 +0300

    Filter expired DSTRootCAX3
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const https = require('https');
 const formatter = require('./lib/formatter');
 
+const DSTRootCAX3 = 
+"-----BEGIN CERTIFICATE-----\n" +
+"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/MSQwIgYDVQQK\n" +
+"ExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMTDkRTVCBSb290IENBIFgzMB4X\n" +
+"DTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVowPzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1\n" +
+"cmUgVHJ1c3QgQ28uMRcwFQYDVQQDEw5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQAD\n" +
+"ggEPADCCAQoCggEBAN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmT\n" +
+"rE4Orz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEqOLl5CjH9\n" +
+"UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9bxiqKqy69cK3FCxolkHRy\n" +
+"xXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40d\n" +
+"utolucbY38EVAjqr2m7xPi71XAicPNaDaeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0T\n" +
+"AQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQ\n" +
+"MA0GCSqGSIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69ikug\n" +
+"dB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXrAvHRAosZy5Q6XkjE\n" +
+"GB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZzR8srzJmwN0jP41ZL9c8PDHIyh8bw\n" +
+"RLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubS\n" +
+"fZGL+T0yjWW06XyxV3bqxbYoOb8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n" +
+"-----END CERTIFICATE-----\n";
+
 if (process.platform !== 'darwin') {
   module.exports.all = () => [];
   module.exports.each = () => {};
@@ -28,7 +47,14 @@ else {
 
   const all = allTrusted.concat(allRoot);
 
-  all.filter(duplicated).forEach(cert => ca.push(cert));
+  const noDuplicated = all.filter(duplicated);
+  const noExpired = noDuplicated.filter(cert => !cert.includes(DSTRootCAX3));
+
+  if (noExpired.length < noDuplicated) {
+    console.log('[MAC-CA]: Filtered expired CA');
+  }
+
+  noExpired.forEach(cert => ca.push(cert));
 
   module.exports.der2 = formatter.validFormats;
 
